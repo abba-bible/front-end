36d6a99d886ec2d639706160205a6e69e7e72188
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-10-07 22:17:36 -0400

    another stash test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.js';
 import'./styles/style.css';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
