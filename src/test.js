dcd7800de3860c52ebf9aa0dd5fcaaa3476b52e2
Ron Hughes <ronald.hughes1982@gmail.com>
2019-10-09 21:17:24 -0400

    axios requests

--- src/test.js
+++ src/test.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import * as blockstack from 'blockstack';
+import axios from 'axios';
 
 
 export default function Test(props) {
+
+    let proxy = "https://cors-anywhere.herokuapp.com/";
+    let url  = "http://getbible.net/json?p=James"
+
+    useEffect((e) => {
+        axios.get(proxy + url)
+        .then((res) => {
+            var output = res.data
+            console.log(output.book)})
+        .catch((err) => {})
+    }, [])
+
     return(
         <div>
-            {console.log(blockstack)}
-            {console.log(props.userSession)}
+            {/* {console.log(blockstack)}
+            {console.log(props.userSession)} */}
+
         </div>
     )
 
